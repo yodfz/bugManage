26b6cffe68ebc503d0fa0c1e2b3541dbc4735709
yodfz <yodfz@qq.com>
2016-09-11 16:57:02 +0800

    管理界面

--- server/src/layouts/MainLayout/indexLayout.jsx
+++ server/src/layouts/MainLayout/indexLayout.jsx
@@ -9,7 +9,7 @@ const template = ({children}) =>{
       <div className="ant-layout-logo"></div>
       <Menu mode="inline" theme="dark"
             defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']}>
-        <SubMenu key="sub1" title={<span><Icon type="user" />基本配置</span>}>
+        <SubMenu key="sub1" title={<span><Icon type="setting" />基本配置</span>}>
           <Menu.Item key="1">站点配置</Menu.Item>
           <Menu.Item key="2">短信配置</Menu.Item>
           <Menu.Item key="3">邮件配置</Menu.Item>
