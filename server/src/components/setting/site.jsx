20ef6d28b1b6be8674e07664a770d0a623e78856
yodfz <yodfz@qq.com>
2016-09-20 16:23:25 +0800

    UI设计

--- server/src/components/setting/site.jsx
+++ server/src/components/setting/site.jsx
@@ -1,13 +1,28 @@
-import React, { Component, PropTypes } from 'react';
-import { Menu, Breadcrumb, Icon, Row , Col , Badge } from 'antd';
+import React, { Component, PropTypes,Props } from 'react';
+import { Menu, Breadcrumb, Icon, Row , Col , Badge,Form, Input, Button, Checkbox, Radio, Tooltip } from 'antd';
 import DefaultTemplate from '../../layouts/MainLayout/defaultTemplate';
 import styles from './site.less';
 
 const template = () => {
+  const { getFieldProps } = Props.form;
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('收到表单值：', this.props.form.getFieldsValue());
+  };
+
   return (
     <DefaultTemplate>
-      <div className="main">
-        站点配置
+      <div className="site">
+        <Form onSubmit={handleSubmit}>
+          <FormItem
+            label="站点名称"
+          >
+            <Input placeholder="请输入站点名称"
+              {...getFieldProps('userName')}
+            />
+          </FormItem>
+        </Form>
       </div>
     </DefaultTemplate>
   );
